757ab3e24f129d86955e576a3ce03a169f7077e9
Rexella G. Swan <rgswanllc1@gmail.com>
2023-02-06 03:59:20 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
+
+import "./App.css";
 
 function Validator(props) {
   //useState to trigger a response once something happens
   const [creditCard, validateCreditCard] = useState("");
-  const [valid, confValid] = useState(null);
+  const [valid, confValid] = useState(NaN);
 
   useEffect(() => {
-  
     const validCard = (creditCard) => {
-
       let addNums = 0;
       let checker = false;
       //iterate through
       for (let i = creditCard.length - 1; i >= 0; i--) {
-        //convert to a string 
+        //convert from a string
         let numbers = parseInt(creditCard.charAt(i));
-        if (checker === true) {numbers *= 2;}
+        if (checker === true) {
+          numbers *= 2;
+        }
         //boolean with conditional ? true : false
         addNums += numbers > 9 ? numbers - 9 : numbers;
         //if checker changes
         checker = !checker;
       }
       return addNums % 10 === 0;
-      
     };
-    
     confValid(validCard(creditCard));
   }, [creditCard]);
 
-// function input (){
-//     if (valid === true) {
-//        return <p>Valid entry! Card information has been accepted.</p>;
-//     }
-//   }
+  // function input (){
+  //     if (valid === true) {
+  //        return <p>Valid entry! Card information has been accepted.</p>;
+  //     }
+  //   }
   // input()
 
   return (
     <div>
-
-          <p>Enter Card Information </p>
-          <br/>
-           <input value={creditCard} onChange={(func) => validateCreditCard(func.target.value)}/>
-           <br/>
-{/* /* Booleans in react must be formated diffrent in react*/ }
-      {valid === true ? "Valid entry! Card information has been accepted.": null}
-      {valid === false ? "Error! Invalid entry, please try again." : null}
+      <form>
+      <p>Enter Card Information </p>
+      <input 
+        value={creditCard}
+        onChange={(event) => validateCreditCard(event.target.value) }
+      />
+      {/* <button value={creditCard} onClick={(event) => validateCreditCard(event.target.value) } type='submit'>Submit!</button> */}
+      </form>
+      <h1>{valid === true
+        ? "Valid entry! Card information has been accepted."
+        : "Error! Invalid entry, please try again."}
+        </h1>
+      {/* /* Booleans in react must be formated diffrent in react*/}
+      
     </div>
   );
 }
